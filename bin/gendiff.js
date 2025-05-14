fa1bc2117c249b23476cf9f256e1d88fdfc3ec74
Oksana1992-hash <nox23061992@gmail.com>
2025-05-14 13:34:42 +0300

    add arguments & options

--- bin/gendiff.js
+++ bin/gendiff.js
@@ -3,8 +3,12 @@
 import { Command } from 'commander';
 
 const program = new Command();
+
 program
     .description('Compares two configuration files and shows a difference.')
-    .option('-V, --version', 'output the version number')
+    .version('1.0.0')
+    .argument('filepath1')
+    .argument('filepath2')
+    .option('-f, --format [type]', 'output format')
 
 program.parse();
