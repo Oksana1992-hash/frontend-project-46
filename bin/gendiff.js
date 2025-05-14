c958bdfc8817264e11553d47679c1101c4b92b94
Oksana1992-hash <nox23061992@gmail.com>
2025-05-14 22:59:18 +0300

    add name & action

--- bin/gendiff.js
+++ bin/gendiff.js
@@ -1,14 +1,19 @@
 #!/usr/bin/env node
 
 import { Command } from 'commander';
+import app from '../src/index.js';
 
 const program = new Command();
 
 program
+    .name('gendiff')
     .description('Compares two configuration files and shows a difference.')
     .version('1.0.0')
-    .argument('filepath1')
-    .argument('filepath2')
+    .argument('<filepath1>')
+    .argument('<filepath2>')
     .option('-f, --format [type]', 'output format')
+    .action((a, b, options) => {
+        console.log(app(a, b, options));
+    });
 
 program.parse();
