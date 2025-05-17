9eed4dff529dbbe0022e494da399b828bf0b0275
Oksana1992-hash <nox23061992@gmail.com>
2025-05-17 23:31:40 +0300

    fixed linter errors

--- __tests__/gendiff.test.js
+++ __tests__/gendiff.test.js
@@ -1,12 +1,10 @@
-import gendiff from '../src/index.js';
 import path from 'path';
 import fs from 'fs';
+import gendiff from '../src/index.js';
 
 const getFixturesPath = (filename) => path.resolve('__fixtures__', filename);
 const result = fs.readFileSync(getFixturesPath('result.txt'), 'utf-8').trim();
 
 test('gendiff', () => {
-  expect(gendiff(
-    getFixturesPath('file1.json').trim(), getFixturesPath('file2.json'))
-  ).toEqual(result);
-})
+  expect(gendiff(getFixturesPath('file1.json').trim(), getFixturesPath('file2.json'))).toEqual(result);
+});
--- bin/gendiff.js
+++ bin/gendiff.js
@@ -6,14 +6,14 @@ import app from '../src/index.js';
 const program = new Command();
 
 program
-    .name('gendiff')
-    .description('Compares two configuration files and shows a difference.')
-    .version('1.0.0')
-    .argument('<filepath1>')
-    .argument('<filepath2>')
-    .option('-f, --format [type]', 'output format')
-    .action((a, b, options) => {
-        console.log(app(a, b, options));
-    });
+  .name('gendiff')
+  .description('Compares two configuration files and shows a difference.')
+  .version('1.0.0')
+  .argument('<filepath1>')
+  .argument('<filepath2>')
+  .option('-f, --format [type]', 'output format')
+  .action((a, b, options) => {
+    console.log(app(a, b, options));
+  });
 
 program.parse();
