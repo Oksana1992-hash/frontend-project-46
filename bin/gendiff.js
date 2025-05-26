8d899cffb0f20309577ae710bd2a954cbbee9f78
Oksana1992-hash <nox23061992@gmail.com>
2025-05-27 00:43:41 +0300

    Added stylish as the default formatter

--- bin/gendiff.js
+++ bin/gendiff.js
@@ -1,19 +1,19 @@
 #!/usr/bin/env node
 
 import { Command } from 'commander';
-import app from '../src/index.js';
+import gendiff from '../src/index.js';
 
 const program = new Command();
 
 program
   .name('gendiff')
   .description('Compares two configuration files and shows a difference.')
-  .version('1.0.0')
+  .version('1.0.0', '-v, --vers', 'output the current version')
   .argument('<filepath1>', 'path to the first file')
   .argument('<filepath2>', 'path to the second file')
-  .option('-f, --format [type]', 'output format', 'stylish')
-  .action((a, b, options) => {
-    console.log(app(a, b, options));
+  .option('-f, --format [type]', 'output format (default: "stylish")')
+  .action((filepath1, filepath2, option) => {
+    console.log(gendiff(filepath1, filepath2, option.format));
   });
 
 program.parse();
