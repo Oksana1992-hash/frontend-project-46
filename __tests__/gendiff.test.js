3f92149fef2ab74b0d2e502a2a269d3f998d07f0
Oksana1992-hash <nox23061992@gmail.com>
2025-05-30 13:06:22 +0300

    added test for nested structures with output in the "Json" format

--- __tests__/gendiff.test.js
+++ __tests__/gendiff.test.js
@@ -19,16 +19,23 @@ describe('formatters', () => {
 
   const stylishDiff = fs.readFileSync(getFixturesPath('stylish.txt'), 'utf-8').trim();
   const plainDiff = fs.readFileSync(getFixturesPath('plain.txt'), 'utf-8').trim();
+  const jsonDiff = fs.readFileSync(getFixturesPath('json.txt'), 'utf-8').trim();
 
-  test('gendiff for nested structures with output "stylish"', () => {
+  test('gendiff for nested structures with output in the "stylish" format"', () => {
     expect(gendiff(fileJsonPath1, fileJsonPath2)).toEqual(stylishDiff);
     expect(gendiff(fileYmlPath1, fileYmlPath2)).toEqual(stylishDiff);
     expect(gendiff(fileYamlPath1, fileYamlPath2)).toEqual(stylishDiff);
   });
 
-  test('gendiff for nested structures with output "plain"', () => {
+  test('gendiff for nested structures with output in the "plain" format', () => {
     expect(gendiff(fileJsonPath1, fileJsonPath2, 'plain')).toEqual(plainDiff);
     expect(gendiff(fileYmlPath1, fileYmlPath2, 'plain')).toEqual(plainDiff);
     expect(gendiff(fileYamlPath1, fileYamlPath2, 'plain')).toEqual(plainDiff);
+  });
+
+  test('gendiff for nested structures with output in the "json" format', () => {
+    expect(gendiff(fileJsonPath1, fileJsonPath2, 'json')).toEqual(jsonDiff);
+    expect(gendiff(fileYmlPath1, fileYmlPath2, 'json')).toEqual(jsonDiff);
+    expect(gendiff(fileYamlPath1, fileYamlPath2, 'json')).toEqual(jsonDiff);
   });
 });
