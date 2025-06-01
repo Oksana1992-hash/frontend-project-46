9564e3295c0f5f18cc1a4c9893f693b141991495
Oksana1992-hash <nox23061992@gmail.com>
2025-06-01 23:50:26 +0300

    fixed linter errors

--- src/parser.js
+++ src/parser.js
@@ -1,13 +1,13 @@
 import yaml from 'js-yaml';
 
 export default (data, format) => {
-    if (format === '.json') {
-        return JSON.parse(data);
-    }
+  if (format === '.json') {
+    return JSON.parse(data);
+  }
 
-    if (format === '.yml' || format === '.yaml') {
-        return yaml.load(data);
-    }
+  if (format === '.yml' || format === '.yaml') {
+    return yaml.load(data);
+  }
 
-    throw Error('unknow extension');
+  throw Error('unknow extension');
 };
