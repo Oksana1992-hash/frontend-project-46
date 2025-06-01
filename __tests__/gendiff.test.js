effd7580a3e943fe272f6de71d9ff8085cbf891d
Oksana1992-hash <nox23061992@gmail.com>
2025-06-01 23:08:24 +0300

    added tests

--- __tests__/gendiff.test.js
+++ __tests__/gendiff.test.js
@@ -1,7 +1,7 @@
 import path, { dirname } from 'path';
 import fs from 'fs';
 import { fileURLToPath } from 'url';
-import { expect, test } from '@jest/globals';
+import { describe, expect, test } from '@jest/globals';
 import gendiff from '../src/index.js';
 
 const __filename = fileURLToPath(import.meta.url);
@@ -9,33 +9,57 @@ const __dirname = dirname(__filename);
 
 const getFixturesPath = (filename) => path.resolve(__dirname, '..', '__fixtures__', filename);
 
-describe('formatters', () => {
-  const fileJsonPath1 = getFixturesPath('file1.json');
-  const fileJsonPath2 = getFixturesPath('file2.json');
-  const fileYamlPath1 = getFixturesPath('file1.yaml');
-  const fileYamlPath2 = getFixturesPath('file2.yaml');
-  const fileYmlPath1 = getFixturesPath('file1.yml');
-  const fileYmlPath2 = getFixturesPath('file2.yml');
+const casesFormats = ['json', 'yaml', 'yml'];
+
+describe.each(casesFormats)('main functionality for %s format', (format) => {
+  const filePath1 = getFixturesPath(`file1.${format}`);
+  const filePath2 = getFixturesPath(`file2.${format}`);
 
   const stylishDiff = fs.readFileSync(getFixturesPath('stylish.txt'), 'utf-8').trim();
   const plainDiff = fs.readFileSync(getFixturesPath('plain.txt'), 'utf-8').trim();
   const jsonDiff = fs.readFileSync(getFixturesPath('json.txt'), 'utf-8').trim();
 
-  test('gendiff for nested structures with output in the "stylish" format"', () => {
-    expect(gendiff(fileJsonPath1, fileJsonPath2)).toEqual(stylishDiff);
-    expect(gendiff(fileYmlPath1, fileYmlPath2)).toEqual(stylishDiff);
-    expect(gendiff(fileYamlPath1, fileYamlPath2)).toEqual(stylishDiff);
+  test('generates correct diff in default style (stylish)', () => {
+    expect(gendiff(filePath1, filePath2)).toEqual(stylishDiff);
   });
 
-  test('gendiff for nested structures with output in the "plain" format', () => {
-    expect(gendiff(fileJsonPath1, fileJsonPath2, 'plain')).toEqual(plainDiff);
-    expect(gendiff(fileYmlPath1, fileYmlPath2, 'plain')).toEqual(plainDiff);
-    expect(gendiff(fileYamlPath1, fileYamlPath2, 'plain')).toEqual(plainDiff);
+  test('generates correct diff in plain format', () => {
+    expect(gendiff(filePath1, filePath2, 'plain')).toEqual(plainDiff);
   });
 
-  test('gendiff for nested structures with output in the "json" format', () => {
-    expect(gendiff(fileJsonPath1, fileJsonPath2, 'json')).toEqual(jsonDiff);
-    expect(gendiff(fileYmlPath1, fileYmlPath2, 'json')).toEqual(jsonDiff);
-    expect(gendiff(fileYamlPath1, fileYamlPath2, 'json')).toEqual(jsonDiff);
+  test('generates correct diff in json format', () => {
+    expect(gendiff(filePath1, filePath2, 'json')).toEqual(jsonDiff);
   });
+
+  const noChangesStylishResult = fs.readFileSync(getFixturesPath('noChangesStylishResult.txt'), 'utf-8').trim();
+  const noChangesPlainResult = fs.readFileSync(getFixturesPath('noChangesPlainResult.txt'), 'utf-8').trim();
+  const noChangesJsonResult = fs.readFileSync(getFixturesPath('noChangesJsonResult.txt'), 'utf-8').trim();
+
+  test('no differences for identical files - stylish', () => {
+    expect(gendiff(filePath1, filePath1)).toEqual(noChangesStylishResult);
+  });
+
+  test('no differences for identical files - plain', () => {
+    expect(gendiff(filePath1, filePath1, 'plain')).toEqual(noChangesPlainResult);
+  });
+
+  test('no differences for identical files - json', () => {
+    expect(gendiff(filePath1, filePath1, 'json')).toEqual(noChangesJsonResult);
+  });
+});
+
+test('throws error if file does not exist', () => {
+  expect(() => gendiff('nonexistent1.json', 'nonexistent2.json')).toThrow();
+});
+
+test('throws error for unsupported file extension', () => {
+  const unsupportedFilePath = getFixturesPath('unsupported.txt');
+
+  expect(() => gendiff(unsupportedFilePath, unsupportedFilePath)).toThrow('unknow extension');
+});
+
+test('testing "The specified format is not supported"', () => {
+  expect(() => {
+    gendiff(getFixturesPath('file1.json'), getFixturesPath('file2.json'), 'txt');
+  }).toThrow('txt is not support');
 });
