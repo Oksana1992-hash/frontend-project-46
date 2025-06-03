b9c1f3d8fe5a4fba965a923c323f737dc73d0aa0
Oksana1992-hash <nox23061992@gmail.com>
2025-06-03 15:03:19 +0300

    the "switch-case" design has been replaced with a "table-based dispatch"

--- src/formatters/plain.js
+++ src/formatters/plain.js
@@ -21,29 +21,27 @@ const fullPath = (path, key) => {
 
 const genPlainFormat = (tree) => {
   const iter = (node, path = '') => {
+    const statusHandlers = {
+      added: (path, { key, value }) => `Property '${fullPath(path, key)}' was added with value: ${formatValue(value)}`,
+      removed: (path, { key }) => `Property '${fullPath(path, key)}' was removed`,
+      modified: (path, { key, oldValue, newValue }) => `Property '${fullPath(path, key)}' was updated. From ${formatValue(oldValue)} to ${formatValue(newValue)}`,
+      unchanged: () => [],
+      nested: (path, { key, value }, iter) => iter(value, fullPath(path, key)),
+    };
+
     const lines = node.flatMap((item) => {
-      const {
-        key, value, oldValue, newValue, status,
-      } = item;
+      const handler = statusHandlers[item.status];
 
-      switch (status) {
-        case 'added':
-          return `Property '${fullPath(path, key)}' was added with value: ${formatValue(value)}`;
-        case 'removed':
-          return `Property '${fullPath(path, key)}' was removed`;
-        case 'modified':
-          return `Property '${fullPath(path, key)}' was updated. From ${formatValue(oldValue)} to ${formatValue(newValue)}`;
-        case 'unchanged':
-          return [];
-        case 'nested':
-          return iter(value, fullPath(path, key));
-        default:
-          throw new Error(`Status ${status} is not supported`);
+      if (!handler) {
+        throw new Error(`Status ${item.status} is not supported`);
       }
+      return handler(path, item, iter);
     });
-    return `${lines.join('\n')}`;
-  };
-  return iter(tree, '');
-};
 
+    return lines;
+  };
+
+  const resultLines = iter(tree, '');
+  return resultLines.join('\n');
+};
 export default genPlainFormat;
